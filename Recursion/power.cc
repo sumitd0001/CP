5f184d8a5f3f4cbb2ab2662f47977df058c9b48a
Sumit Dahiya <SUMIT.DAHIYA@ibm.com>
2024-01-30 14:56:39 +0530

    updated power from O(n) to O(logn)

--- Recursion/power.cc
+++ Recursion/power.cc
@@ -4,9 +4,28 @@ using namespace std;
 #define ll long long
 #define mod 1000000007
 
+
+/*
+O(n)
+*/
 int power (int n, int a) {
     if (a == 0) return 1;
     return n*power(n, a-1);
+}
+
+
+/*
+2^6 = (2^6/2)^2 = (2^3)^2 = 2((2^2)^2)
+O(logn)
+*/
+int fastPower (int n, int a) {
+    if (a == 0) return 1;
+
+    int p = fastPower(n, a/2); // if a is even
+    int psq = p*p;
+    if (a%2 != 0) // if a is odd
+        psq = n*psq;
+    return psq;
 }
 
 void solve() {
@@ -14,6 +33,7 @@ void solve() {
     cout<<"Enter a number and its power : \n";
     cin>>n>>a;
     cout<<"Power of number "<<n<<" to the "<<a<<" : "<<power(n, a)<<"\n";
+    cout<<"Power of number "<<n<<" to the "<<a<<" (using fastPower): "<<fastPower(n, a)<<"\n";
 }
 
 int main() {
